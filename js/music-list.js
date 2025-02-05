4f88dcd0561cb0cb8740f42c5f8b122f4b7a8b8a
luciaduque248 <luciaduque248@gmail.com>
2025-02-04 20:45:58 -0500

    Music and Albums was modified

--- js/music-list.js
+++ js/music-list.js
@@ -1,37 +1,37 @@
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
+    name: "El Doctorado",
+    artist: "Tony Dize Ft. Don Omar & Ken-Y",
     img: "music-1",
     src: "music-1"
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
+    name: "PURRITO APA",
+    artist: "Feid",
     img: "music-2",
     src: "music-2"
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
+    name: "Guaya Guaya",
+    artist: "Don Omar",
     img: "music-3",
     src: "music-3"
   },
   {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
+    name: "Dile Que Tu Me Quieres",
+    artist: "Ozuna",
     img: "music-4",
     src: "music-4"
   },
   {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
+    name: "Río",
+    artist: "J Balvin",
     img: "music-5",
     src: "music-5"
   },
   {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
+    name: "Love Again",
+    artist: "Dua Lipa",
     img: "music-6",
     src: "music-6"
   },
